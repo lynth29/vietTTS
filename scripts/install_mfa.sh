b62c4795f25b9973d63fd6f2e12c7a2e173ff7c9
Ly Nguyen <ly.nguyen@cmetric.vn>
2021-12-28 14:30:41 +0700

    Link and installation file depends on os

--- scripts/install_mfa.sh
+++ scripts/install_mfa.sh
@@ -7,11 +7,19 @@ mkdir -p $root_dir
 cd $root_dir
 
 # download miniconda3
-mac="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
-linux="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-wget -q --show-progress https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-bash Miniconda3-latest-MacOSX-x86_64.sh -b -p $root_dir/miniconda3 -f
-rm Miniconda3-latest-MacOSX-x86_64.sh
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  link="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+  filename="Miniconda3-latest-Linux-x86_64.sh"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  link="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+  filename="Miniconda3-latest-MacOSX-x86_64.sh"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  link="https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe"
+  filename="Miniconda3-latest-Windows-x86_64.exe"
+fi
+wget -q --show-progress $link
+bash $filename -b -p $root_dir/miniconda3 -f
+rm $filename
 
 # create env and install mfa
 $root_dir/miniconda3/bin/conda create -n aligner -c conda-forge montreal-forced-aligner python=3.8 openfst pynini ngram baumwelch -y
