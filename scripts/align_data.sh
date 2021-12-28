9ae13f88e77ffe735ba251a748f82d444756fdd3
Ly Nguyen <ly.nguyen@cmetric.vn>
2021-12-28 14:01:34 +0700

    Add more print

--- scripts/align_data.sh
+++ scripts/align_data.sh
@@ -5,18 +5,20 @@ echo "Install MFA"
 bash ./scripts/install_mfa.sh $1
 
 # Create dirs in train_data
+echo "======="
 echo "Preparing contents to align dataset..."
 mkdir ./train_data/content
 mkdir ./train_data/content/wavs
 mkdir ./train_data/content/aligned
 
-# Create words.txt files
+# Create words.txt file
 python -m train_data.create_words
+# Create lexicon.txt file
 python -m train_data.preparing_speech_corpus
 
 echo "Finish preparing contents to align dataset into pretrained dataset"
 echo "======="
-# activate MFA
+# activate MFA and align dataset
 echo "Activate MFA and align dataset"
 source $1/miniconda3/bin/activate aligner; mfa train --clean -C ./train_data/content/wavs ./train_data/content/lexicon.txt ./train_data/content/aligned
 echo "Finish aligning dataset"
--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -1,5 +1,5 @@
 # train_data dir
-data_root="./train_data/wavs" # modify this
+data_root="./train_data/wavs" # modify this if change dir
 # GGdrive id
 ggdrive_id=$1
 # Make dir
@@ -7,6 +7,8 @@ pushd .
 mkdir -p $data_root
 cd $data_root
 # Download
+echo "Downloading dataset..."
 gdown --id $1 -O fileThuAm.zip
 unzip -q fileThuAm.zip
+echo "Unzipped dataset"
 popd
