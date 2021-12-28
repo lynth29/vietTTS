6541a0244e53b59fd388bfadaceb35b344bd6005
Ly Nguyen <ly.nguyen@cmetric.vn>
2021-12-28 09:51:07 +0700

    Add create_words.py to flow

--- scripts/align_data.sh
+++ scripts/align_data.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-## a script to run Montreal Forced Aligner (MFA) installation file
+# A script to run Montreal Forced Aligner (MFA) installation file
 echo "Install MFA"
 bash ./scripts/install_mfa.sh $1
 
-# Prepare contents to align data
+# Create dirs in train_data
 echo "Preparing contents to align dataset..."
-mkdir ./train_data/content
-mkdir ./train_data/content/wavs
-mkdir ./train_data/content/test
-cat ./assets/infore/lexicon.txt | cut -f 1 > ./train_data/content/words.txt
+mkdir ./train_data/contents
+mkdir ./train_data/contents/wavs
+mkdir ./train_data/contents/test
+
+# Create words.txt files
+python -m train_data.create_words
 python -m train_data.preparing_speech_corpus
 
 echo "Finish preparing contents to align dataset into pretrained dataset"
 echo "======="
 # activate MFA
 echo "Activate MFA and align dataset"
-source $1/miniconda3/bin/activate aligner; mfa train --clean -C ./train_data/content/wavs ./train_data/content/dictionary.txt ./train_data/content/test
+source $1/miniconda3/bin/activate aligner; mfa train --clean -C ./train_data/contents/wavs ./train_data/contents/dictionary.txt ./train_data/contents/test
 echo "Finish aligning dataset"
 conda deactivate
 echo "Deactivate MFA"
