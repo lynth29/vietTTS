53a1baf5743775fda57fc67646ec78f266ec767c
Ly Nguyen <lynth.work@gmail.com>
2022-01-04 10:25:21 +0700

    Change flow

--- scripts/align_data.sh
+++ scripts/align_data.sh
@@ -1,10 +1,7 @@
-# activate MFA and align dataset
-echo "Activate MFA"
-source /miniconda3/bin/activate aligner
 echo "Validating dataset"
-mfa validate ./train_data/content/wavs ./train_data/content/lexicon.txt
+source /miniconda3/bin/activate aligner; mfa validate ./train_data/content/wavs ./train_data/content/lexicon.txt
 echo "Denoising and aligning"
-mfa train --clean -C ./train_data/content/wavs ./train_data/content/lexicon.txt ./train_data/content/aligned
+mfa train --clean ./train_data/content/wavs ./train_data/content/lexicon.txt ./train_data/content/aligned
 echo "Finish aligning dataset"
 conda deactivate
 echo "Deactivate MFA"
