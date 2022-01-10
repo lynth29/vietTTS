bdf8c3773f737c599acbc166ff88856a5fd32dac
Ly Nguyen <lynth.work@gmail.com>
2022-01-10 17:19:11 +0700

    test load_phonemes

--- vietTTS/nat/data_loader.py
+++ vietTTS/nat/data_loader.py
@@ -12,7 +12,7 @@ def load_phonemes_set_from_lexicon_file(fn: Path):
     S = set()
     for line in open(fn, "r").readlines():
         word, phonemes = line.strip().lower().split("\t")
-        phonemes = phonemes.split()
+        phonemes = phonemes.split(' ')
         S.update(phonemes)
 
     S = FLAGS.special_phonemes + sorted(list(S))
